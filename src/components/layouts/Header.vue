f94abd51d8c656c2bdda4abcdd083e29551c4a77
Babatunde Dada <tunde.dada@iisysgroup.com>
2021-05-23 05:08:20 +0100

    added more styling

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -7,17 +7,19 @@
       flat
       height = "80"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <div class="d-flex align-center ml-2">
+        <router-link to="/">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
+        </router-link>
       </div>
-      <h3 class="ml-2">Coinage</h3>
+      <h3 class="ml-2 "> <router-link to="/"> <span class="site-name">Coinage</span> </router-link></h3>
       <v-spacer></v-spacer>
 
       <v-btn
@@ -28,5 +30,9 @@
       </v-btn>
     </v-app-bar>
     </v-container>
- </template>
+ </template><style scoped>
+ .site-name{
+   color: white;
+ }
+ </style>
  
